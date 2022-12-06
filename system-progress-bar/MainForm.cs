ba311c30836521292cc202cffc2cb31a8f03cba4
Tom Gregor <tgregor@ivsoftware.com>
2022-12-06 13:07:40 -0700

    ProgressBar with custom color

--- system-progress-bar/MainForm.cs
+++ system-progress-bar/MainForm.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Numerics;
 using System.Xml.Schema;
+using System.Runtime.InteropServices;
 
 namespace system_progress_bar
 {
@@ -11,18 +13,27 @@ namespace system_progress_bar
             buttonUnzip.Click += onButtonUnzip;
         }
 
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            SetWindowTheme(progressBar1.Handle, string.Empty, string.Empty);
+            progressBar1.ForeColor = Color.Aqua;
+        }
+
+        [DllImport("uxtheme", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        public extern static Int32 SetWindowTheme(IntPtr hWnd,
+                      String textSubAppName, String textSubIdList);
+
 
         private async void onButtonUnzip(object? sender, EventArgs e)
         {
             progressBar1.Visible = true;
-            Progress<int> progress = new Progress<int>(callback);
+            progressBar1.Value = 0;
+            Progress<int> progress = new Progress<int>(value => {
+                progressBar1.Value = value; progressBar1.Update();
+            });
             await Task.Run(() => FileUtils.UnzipTo("mockSource", "mockDest", true, progress));
             progressBar1.Visible = false;
-        }
-
-        private void callback(int value)
-        {
-            progressBar1.Value = value;
         }
     }
     class FileUtils
@@ -37,14 +48,13 @@ namespace system_progress_bar
             IProgress<int> progress)
         {
             var files = GetFilesMock();
-            int completed = 1;
+            int complete = 0;
 
-            double total = files.Length;
+            int total = files.Length;
             foreach (var file in files)
             {
                 UnzipOneMock(sourceDir, targetDir, file);
-                var net = (int)(completed++ * 100 / total);
-                progress.Report(net);
+                progress.Report((int)(++complete / (double)total * 100));
             }
         }
     }
